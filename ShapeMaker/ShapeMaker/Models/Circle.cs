dbf147801440df9025c1eae88b17f3712ec8483e
Nikita Filyaeff <nikita.filyaeff@yandex.ru>
2024-03-08 15:04:19 +0300

    init Shape Maker base, and add circle, triangle models

---
+++ ShapeMaker/ShapeMaker/Models/Circle.cs
@@ -0,0 +1,19 @@
+﻿using ShapeMaker.BaseModels;
+
+namespace ShapeMaker.Models
+{
+    public class Circle : IShape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public double GetArea()
+        {
+           return Math.PI * Radius *Radius;
+        }
+    }
+}
---
+++ ShapeMaker/ShapeMaker/Models/Triangle.cs
@@ -0,0 +1,50 @@
+﻿using ShapeMaker.BaseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeMaker.Models
+{
+    public class Triangle : IShape
+    {
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+
+        private double SemiPerimeter => (SideA + SideB + SideC) / 2;
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            this.SideA = sideA;
+            this.SideB = sideB;
+            this.SideC = sideC;
+        }
+
+        public double GetArea()
+        {
+            return Math.Sqrt(SemiPerimeter
+                                         * (SemiPerimeter - SideA)
+                                         * (SemiPerimeter - SideB)
+                                         * (SemiPerimeter - SideC));
+        }
+
+        public bool IsRectangular()
+        {
+            double a = Math.Pow(SideA, 2);
+            double b = Math.Pow(SideB, 2);
+            double c = Math.Pow(SideC, 2);
+
+            return CheckSides(a + b, c)
+                   || CheckSides(a + c, b)
+                   || CheckSides(b + c, a);
+
+        }
+
+        private bool CheckSides(double sumKatet, double hypotenuse)
+        {
+            return hypotenuse - sumKatet == 0 ? true : false;
+        }
+    }
+}
